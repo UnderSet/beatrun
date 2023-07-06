8b871b731a2c2cdf2281d5e7aefacec879e1d499
Lam Hung <lamhung12700@gmail.com>
2023-07-06 19:00:36 +0700

    Tiny little muckup

--- beatrun-jonny2/gamemodes/beatrun-underset/gamemode/cl_init.lua
+++ beatrun-jonny2/gamemodes/beatrun-underset/gamemode/cl_init.lua
@@ -1,5 +1,5 @@
 include("shared.lua")
 
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME", "nameasc")) do
+for _, v in ipairs(file.Find("gamemodes/beatrun-underset/gamemode/cl/*.lua", "GAME", "nameasc")) do
 	include("cl/" .. v)
 end
